b37e84536dc473a0881e71195ed7d718915509a7
Roger Liu <rogliu26@gmail.com>
2016-02-13 15:26:32 -0500

    pushed instructions for installing NLTK on windows. Also added basic examples of NLTK functions in the Question Generating Module

--- Question Generating/QuestionStarterFile.py
+++ Question Generating/QuestionStarterFile.py
@@ -1,1 +1,11 @@
-#starter question file
+import nltk
+from pattern.en import parse
+
+#Testing the abilities of nltk
+testString = "I made a poop in my pants"
+
+#part of speech tagging
+print(nltk.pos_tag(nltk.word_tokenize(testString)))
+
+#parse tree tests
+print(parse(testString, relations=True, lemmata=True))
