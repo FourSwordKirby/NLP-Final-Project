31d7f286d8c6133588abd2addbc30036276ad8cb
gold <gamma999@gmail.com>
2016-02-26 15:48:18 -0500

    Stubs for question part doen

--- Question Generating/GenerateQuestions.py
+++ Question Generating/GenerateQuestions.py
@@ -7,19 +7,29 @@ desc:
 """
 
 import sys
-from inc.ArticleParser import *
+from inc.ArticleParser import ingest, augment
+from inc.Questions import articleToQuestion, bestQuestion
 
 # Generates numQuestions questions about the article in fileName
 def main(fileName, numQuestions):
 
-    # Not going to validate arguments.
-    articleFile = open(fileName, 'r')
+    # take in the file, return a dict of sentences where the key is the
+    # heading and the value is the list of sentences.
+    article = ingest(fileName)
 
-    # NYI
-    print asfd()
+    # for each sentence in sentences, form a list of augmented sentences
+    for (heading, paragraph) in article.iteritems():
+        article[heading] = augment(paragraph)
 
-    # Clean up.
-    articleFile.close()
+    # Transforms the sentences in the article into a list of questions.
+    questions = articleToQuestion(article)
+
+    # Find the top questions
+    bestQuestions = bestQuestion(questions, numQuestions)
+
+    # Print the best questions
+    for question in bestQuestions:
+        print question
 
 # Run main()!
 if __name__ == '__main__':
---
+++ Question Generating/inc/Questions.py
@@ -0,0 +1,18 @@
+"""
+author:
+
+    etctec
+desc:
+"""
+
+#import rules
+
+# Takes in a dict of {heading : paragraph} and returns a list of questions.
+def articleToQuestion(article):
+    # NYI
+    return []
+
+# Returns the top num questions in questions.
+def bestQuestion(questions, num):
+    # NYI
+    return []
